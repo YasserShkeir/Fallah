6772065182398248012717c75b87a96ae61cf348
Yasser Shkeir <shkeiryasser@gmail.com>
2022-10-25 10:49:55 +0300

    Backend: Models -> Products Schema

---
+++ fallah-backend/models/products.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  childCategoryID: {
+    type: String,
+    required: "Child Category ID is required",
+  },
+  farmerID: {
+    type: String,
+    required: "Farmer ID is required",
+  },
+  productName: {
+    type: String,
+    required: "Product Name is required",
+  },
+  startingSeason: {
+    type: Date,
+  },
+  endingSeason: {
+    type: Date,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
