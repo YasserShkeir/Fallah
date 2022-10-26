ebb0faeee92d3312ad6c75fef669ee68080f7813
Yasser Shkeir <shkeiryasser@gmail.com>
2022-10-26 18:03:50 +0300

    Backend: Auth Control-> Users Type Registration

--- fallah-backend/controllers/auth.controller.js
+++ fallah-backend/controllers/auth.controller.js
@@ -29,25 +29,38 @@ const register = async (req, res) => {
   const { name, email, phone, userType, password } = req.body;
 
   try {
-    const user = await User.findOne({ email });
+    const user = await User.User.findOne({ email });
     if (user) {
       return res.status(409).send({ message: "User already exists" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = new User({
-      name,
-      email,
-      phone,
-      userType,
-      password: hashedPassword,
-    });
-
-    await newUser.save();
-    res.status(201).send({ message: "User created successfully" });
+    if (userType === "farmer") {
+      const newUser = new User.Farmer({
+        name,
+        email,
+        phone,
+        userType,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.status(201).send({ message: "User created successfully" });
+    } else if (userType === "buyer") {
+      const newUser = new User.Buyer({
+        name,
+        email,
+        phone,
+        userType,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.status(201).send({ message: "User created successfully" });
+    } else {
+      return res.status(400).send({ message: "Invalid user type" });
+    }
   } catch (error) {
-    res.status(500).send({ message: "Server error" });
+    res.status(500).send({ message: "Server error", error: error.message });
   }
 };
 
