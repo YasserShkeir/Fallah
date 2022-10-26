c0189016ffc3991ce2ffba8c7d09a0307bb9ce99
Yasser Shkeir <shkeiryasser@gmail.com>
2022-10-26 20:55:11 +0300

    Backend: Index-> Add User Route

--- fallah-backend/index.js
+++ fallah-backend/index.js
@@ -10,6 +10,9 @@ app.use(cors());
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+const userRoutes = require("./routes/users.routes");
+app.use("/users", userRoutes);
+
 app.listen(process.env.PORT, (err) => {
   if (err) throw err;
   console.log(`Server is running in Port: ${process.env.PORT}`);
