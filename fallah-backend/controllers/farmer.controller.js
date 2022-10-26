87d06b65abc5a150d62df467fda881c3495ea878
Yasser Shkeir <shkeiryasser@gmail.com>
2022-10-26 21:29:22 +0300

    Backend: Farmer Control-> Edit Product

--- fallah-backend/controllers/farmer.controller.js
+++ fallah-backend/controllers/farmer.controller.js
@@ -59,6 +59,66 @@ const registerProduct = async (req, res) => {
   }
 };
 
+const editProduct = async (req, res) => {
+  // Edit a product
+  try {
+    const {
+      // Get the data from the request body
+      id,
+      childCategoryID,
+      productName,
+      images,
+      startingSeason,
+      endingSeason,
+      harvestedOn,
+      pickupLocation,
+      freshnessStatus,
+      measuringUnit,
+      pricePerMeasuringUnit,
+      minBulkAmount,
+      bulkPrice,
+      amountAvailable,
+    } = req.body;
+
+    const farmer = await User.Farmer.findById(req.user._id);
+    // Update Product
+    const product = farmer.products.find(
+      (product) => product._id.toString() === id
+    );
+
+    if (product) {
+      if (childCategoryID) product.childCategoryID = childCategoryID;
+      if (productName) product.productName = productName;
+      if (images) product.images = images;
+      if (startingSeason) product.startingSeason = startingSeason;
+      if (endingSeason) product.endingSeason = endingSeason;
+      if (harvestedOn) product.harvestedOn = harvestedOn;
+      if (pickupLocation) product.pickupLocation = pickupLocation;
+      if (freshnessStatus) product.freshnessStatus = freshnessStatus;
+      if (measuringUnit) product.measuringUnit = measuringUnit;
+      if (pricePerMeasuringUnit)
+        product.pricePerMeasuringUnit = pricePerMeasuringUnit;
+      if (minBulkAmount) product.minBulkAmount = minBulkAmount;
+      if (bulkPrice) product.bulkPrice = bulkPrice;
+      if (amountAvailable) product.amountAvailable = amountAvailable;
+      product.updated_at = Date.now();
+
+      await farmer.save();
+      res.status(201).json({
+        message: "Product updated successfully",
+      });
+    } else {
+      res.status(400).json({
+        message: "Product does not exist",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   registerProduct,
+  editProduct,
 };
