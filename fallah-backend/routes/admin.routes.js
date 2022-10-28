88a678aeb65cecae371e8d97aaca725a60a90b68
Yasser Shkeir <shkeiryasser@gmail.com>
2022-10-28 14:58:53 +0300

    Backend: Route-> Admin add API Routes

--- fallah-backend/routes/admin.routes.js
+++ fallah-backend/routes/admin.routes.js
@@ -5,7 +5,12 @@ const auth = require("../middlewares/auth.middleware");
 const admin = require("../middlewares/admin.middleware");
 
 // Admin Controller
-const { getUsers, editUser } = require("../controllers/admin.controller");
+const {
+  getUsers,
+  editUser,
+  deleteUser,
+  addCategory,
+} = require("../controllers/admin.controller");
 
 // Create a new router
 const router = Router();
@@ -13,5 +18,8 @@ const router = Router();
 // Admin Routes
 router.get("/users", auth, admin, getUsers); // Get all users
 router.put("/users", auth, admin, editUser); // Edit a user
+router.delete("/users", auth, admin, deleteUser); // Delete a user
+
+router.post("/category", auth, admin, addCategory); // Add a category
 
 module.exports = router;
