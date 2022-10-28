8d39df83ecf35031a5bd5205cd6622f761071ad4
Yasser Shkeir <shkeiryasser@gmail.com>
2022-10-28 14:11:36 +0300

    Backend: Control-> Admin Edit User

--- fallah-backend/controllers/admin.controller.js
+++ fallah-backend/controllers/admin.controller.js
@@ -2,15 +2,42 @@ const User = require("../models/user.model");
 
 const getUsers = async (req, res) => {
   try {
-    const users = await User.find();
+    const users = await User.User.find().select("-password");
     res.status(200).json({
       users,
     });
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+const editUser = async (req, res) => {
+  try {
+    const { id, name, email, phone } = req.body;
+    const user = await User.User.findById(id);
+    if (user) {
+      if (name) user.name = name;
+      if (email) user.email = email;
+      if (phone) user.phone = phone;
+      await user.save();
+      res.status(200).json({
+        message: "User updated successfully",
+      });
+    } else {
+      res.status(400).json({
+        message: "User does not exist",
+      });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
   }
 };
 
 module.exports = {
   getUsers,
+  editUser,
 };
