d20579562e7958f5eb499adadf9e8a40a471a887
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-01 00:14:44 +0200

    Frontend: Config-> Tailwind Base Colors Added

--- fallah-frontend/React JS Folder/fallah-frontend-reactjs/tailwind.config.js
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      // --light-green: #23ce6b;
+      // --dark-green: #1e6048;
+      // --cream-white: #f6f8ff;
+      // --peachy-yellow: #f1c24c;
+      "light-green": "#23ce6b",
+      "dark-green": "#1e6048",
+      "cream-white": "#f6f8ff",
+      "peachy-yellow": "#f1c24c",
+    },
   },
   plugins: [],
 };
