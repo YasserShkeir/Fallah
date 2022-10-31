c99dadd50495b99c334b094b8539eddd4aa47e92
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-01 00:15:03 +0200

    Frontend: Pages-> Admin Base Login Page

---
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/login.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const AdminLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>Admin Portal Sign In</h1>
+        <form onSubmit={handleSubmit}>
+          <h5>E-mail</h5>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Sign In</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AdminLogin;
