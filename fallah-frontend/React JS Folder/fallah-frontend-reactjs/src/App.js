19182278ae4195e507fe2f73f289e9b33145c9c2
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-01 00:15:47 +0200

    Frontend: App-> Added Admin Login Route

--- fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/App.js
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
 import ErrorPage from "./Misc/errorPage";
+import AdminLogin from "./Admin/pages/login";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<ErrorPage />} />
+        <Route path="/error" element={<ErrorPage />} />
+        <Route path="/" element={<AdminLogin />} />
       </Routes>
     </div>
   );
