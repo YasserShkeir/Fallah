aa27468a26e896de64dac79b3d5818e90134a6b7
Yasser Shkeir <shkeiryasser@gmail.com>
2022-10-31 21:01:27 +0200

    Frontend: Pages-> Initial Error Page Linking

--- fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/App.js
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
+import ErrorPage from "./Misc/errorPage";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<ErrorPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
