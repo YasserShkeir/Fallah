688c26066beb883704a9356b9a63d0465329a3f0
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-02 01:07:46 +0200

    Frontend: Pages-> Admin Login Page Ready V1

--- fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/login.js
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/login.js
@@ -1,15 +1,51 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import LoadingButton from "@mui/lab/LoadingButton";
+import Textfield from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
 
 import adminLoginBG from "../assets/images/adminLoginBG.jpg";
+import loginFormLogo from "../../Assets/images/Transparent Logos/2-Crop.png";
+
+const theme = createTheme({
+  palette: {
+    lightGreen: {
+      main: "#23ce6b",
+    },
+    creamWhite: {
+      main: "#f6f8ff",
+    },
+  },
+  screens: {
+    sm: "640px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1280px",
+  },
+});
 
 const AdminLogin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
+    setLoading(true);
     e.preventDefault();
-    console.log(email, password);
+    await axios
+      .post("http://localhost:3000/auth/login", {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+    setLoading(false);
   };
 
   return (
@@ -17,26 +53,57 @@ const AdminLogin = () => {
       style={{
         backgroundImage: `url(${adminLoginBG})`,
       }}
-      className="h-screen bg-cover bg-center bg-no-repeat"
+      className="flex h-screen bg-cover bg-center bg-no-repeat justify-center items-center"
     >
-      <div>
-        <h1>Admin Portal Sign In</h1>
-        <form onSubmit={handleSubmit}>
-          <h5>E-mail</h5>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <h5>Password</h5>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="submit">Sign In</button>
-        </form>
-      </div>
+      <ThemeProvider theme={theme}>
+        <Grid
+          container
+          className="bg-light-green rounded-3xl border-4 border-cream-white px-7 py-9 w-fit justify-center items-center gap-10"
+          width={"fit-content"}
+        >
+          <Grid item className="h-44">
+            <img src={loginFormLogo} alt="logo" className="h-full" />
+          </Grid>
+          <div className="flex flex-col gap-6 min-h-full">
+            <p className="text-2xl font-bold text-cream-white ">
+              Admin Portal Sign In
+            </p>
+            <form onSubmit={handleSubmit} className="flex flex-col w-72 gap-8">
+              <Textfield
+                inputProps={{ style: { color: "var(--cream-white)" } }}
+                color="creamWhite"
+                required
+                size="small"
+                label="E-mail"
+                variant="filled"
+                type={"email"}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Textfield
+                inputProps={{ style: { color: "var(--cream-white)" } }}
+                color="creamWhite"
+                required
+                size="small"
+                label="Password"
+                variant="filled"
+                type={"password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <LoadingButton
+                loading={loading}
+                variant="contained"
+                color="creamWhite"
+                className="hover:text-white w-fit self-end"
+                type="submit"
+              >
+                Sign In
+              </LoadingButton>
+            </form>
+          </div>
+        </Grid>
+      </ThemeProvider>
     </div>
   );
 };
