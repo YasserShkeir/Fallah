685d6bfacbe0fe3cf32fe5780d0f269538e2880b
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-02 01:07:18 +0200

    Frontend: Pages-> Initial Landing Page Implementation

---
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/landingPage.js
@@ -0,0 +1,14 @@
+import { React } from "react";
+
+import AdminNavigationBar from "../components/navigationBar";
+
+const AdminLandingPage = () => {
+  return (
+    <div className="flex">
+      <AdminNavigationBar />
+      <h1>Admin Landing Page</h1>
+    </div>
+  );
+};
+
+export default AdminLandingPage;
