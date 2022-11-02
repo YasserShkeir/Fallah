aacfe9e881cf15157c46f0a78e4fdd865665726c
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-02 13:59:26 +0200

    Frontend: Page-> Admin Login Page Remove Local Styling

--- fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/login.js
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/login.js
@@ -1,30 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import LoadingButton from "@mui/lab/LoadingButton";
 import Textfield from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 
 import adminLoginBG from "../assets/images/adminLoginBG.jpg";
 import loginFormLogo from "../../Assets/images/Transparent Logos/2-Crop.png";
-
-const theme = createTheme({
-  palette: {
-    lightGreen: {
-      main: "#23ce6b",
-    },
-    creamWhite: {
-      main: "#f6f8ff",
-    },
-  },
-  screens: {
-    sm: "640px",
-    md: "768px",
-    lg: "1024px",
-    xl: "1280px",
-  },
-});
 
 const AdminLogin = () => {
   const [email, setEmail] = useState("");
@@ -55,55 +37,53 @@ const AdminLogin = () => {
       }}
       className="flex h-screen bg-cover bg-center bg-no-repeat justify-center items-center"
     >
-      <ThemeProvider theme={theme}>
-        <Grid
-          container
-          className="bg-light-green rounded-3xl border-4 border-cream-white px-7 py-9 w-fit justify-center items-center gap-10"
-          width={"fit-content"}
-        >
-          <Grid item className="h-44">
-            <img src={loginFormLogo} alt="logo" className="h-full" />
-          </Grid>
-          <div className="flex flex-col gap-6 min-h-full">
-            <p className="text-2xl font-bold text-cream-white ">
-              Admin Portal Sign In
-            </p>
-            <form onSubmit={handleSubmit} className="flex flex-col w-72 gap-8">
-              <Textfield
-                inputProps={{ style: { color: "var(--cream-white)" } }}
-                color="creamWhite"
-                required
-                size="small"
-                label="E-mail"
-                variant="filled"
-                type={"email"}
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <Textfield
-                inputProps={{ style: { color: "var(--cream-white)" } }}
-                color="creamWhite"
-                required
-                size="small"
-                label="Password"
-                variant="filled"
-                type={"password"}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <LoadingButton
-                loading={loading}
-                variant="contained"
-                color="creamWhite"
-                className="hover:text-white w-fit self-end"
-                type="submit"
-              >
-                Sign In
-              </LoadingButton>
-            </form>
-          </div>
+      <Grid
+        container
+        className="bg-light-green rounded-3xl border-4 border-cream-white px-7 py-9 w-fit justify-center items-center gap-10"
+        width={"fit-content"}
+      >
+        <Grid item className="h-44">
+          <img src={loginFormLogo} alt="logo" className="h-full" />
         </Grid>
-      </ThemeProvider>
+        <div className="flex flex-col gap-6 min-h-full">
+          <p className="text-2xl font-bold text-cream-white ">
+            Admin Portal Sign In
+          </p>
+          <form onSubmit={handleSubmit} className="flex flex-col w-72 gap-8">
+            <Textfield
+              inputProps={{ style: { color: "var(--cream-white)" } }}
+              color="creamWhite"
+              required
+              size="small"
+              label="E-mail"
+              variant="filled"
+              type={"email"}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Textfield
+              inputProps={{ style: { color: "var(--cream-white)" } }}
+              color="creamWhite"
+              required
+              size="small"
+              label="Password"
+              variant="filled"
+              type={"password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <LoadingButton
+              loading={loading}
+              variant="contained"
+              color="creamWhite"
+              className="hover:text-white w-fit self-end"
+              type="submit"
+            >
+              Sign In
+            </LoadingButton>
+          </form>
+        </div>
+      </Grid>
     </div>
   );
 };
