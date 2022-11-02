261b41af135e4c050e2957da2e2bb5136fc901c3
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-02 13:47:20 +0200

    Frontend: App-> Added Global Theme

--- fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/App.js
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/App.js
@@ -1,18 +1,41 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import ErrorPage from "./Misc/errorPage";
 import AdminLogin from "./Admin/pages/login";
 import AdminLandingPage from "./Admin/pages/landingPage";
 
+const theme = createTheme({
+  palette: {
+    lightGreen: {
+      main: "#23ce6b",
+    },
+    creamWhite: {
+      main: "#f6f8ff",
+    },
+  },
+  typography: {
+    fontFamily: "Inter, sans-serif",
+  },
+  screens: {
+    sm: "640px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1280px",
+  },
+});
+
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/error" element={<ErrorPage />} />
-        <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin/home" element={<AdminLandingPage />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/error" element={<ErrorPage />} />
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route path="/admin/home" element={<AdminLandingPage />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
