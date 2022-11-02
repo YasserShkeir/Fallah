c3f9680e19924bdb8febb3efb05d23b22384e49d
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-02 15:49:32 +0200

    Frontend: Page-> Admin Landing Page Quick Stats - Total Users

--- fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/landingPage.js
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/landingPage.js
@@ -1,12 +1,55 @@
-import { React } from "react";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
+
+import { Box } from "@mui/material";
 
 import AdminNavigationBar from "../components/navigationBar";
 
 const AdminLandingPage = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwt");
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+
+      // get all users
+      url: "http://localhost:3000/admin/users",
+      method: "get",
+    };
+    const response = axios(config).then((res) => {
+      setUsers(res.data);
+      console.log(res.data);
+    });
+  }, []);
+
   return (
     <div className="flex">
       <AdminNavigationBar />
-      <h1>Admin Landing Page</h1>
+      <Box className="flex flex-col w-full h-screen bg-cream-white px-10 py-14">
+        <p className="text-3xl">Welcome User</p>
+        <Box className="flex flex-col gap-4 mt-10 bg-light-green border-4 border-dark-green p-4">
+          <p className="text-2xl text-cream-white font-semibold">
+            Quick Statistics:
+          </p>
+          <Box className="flex flex-col gap-4">
+            <p className="text-cream-white font-semibold">
+              Total Users:{" "}
+              <span className="font-normal">
+                {users.users ? users.users.length : "Fetching..."}
+              </span>
+            </p>
+            <p className="text-cream-white font-semibold">
+              Total Orders: <span className="font-normal">100</span>
+            </p>
+            <p className="text-cream-white font-semibold">
+              Total Products: <span className="font-normal">100</span>
+            </p>
+          </Box>
+        </Box>
+      </Box>
     </div>
   );
 };
