14ed0e42dddfabb30f5e3ad4b2e6bf54b62aebac
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-03 23:33:36 +0200

    Frontend: Component-> Visitor Navbar Cleanup

--- fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Visitor/components/navigationBar.js
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Visitor/components/navigationBar.js
@@ -1,21 +1,9 @@
-import {
-  Box,
-  AppBar,
-  Button,
-  useScrollTrigger,
-  Slide,
-  Typography,
-  Toolbar,
-} from "@mui/material";
-import { Container } from "@mui/system";
+import { Box, AppBar, Button, useScrollTrigger, Slide } from "@mui/material";
 
 import logo from "../assets/images/3.png";
 
 function HideOnScroll(props) {
   const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
   const trigger = useScrollTrigger({
     target: window ? window() : undefined,
   });
