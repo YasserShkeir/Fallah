45dc6c8a8ef2df061f192383ed2a863ebe123ea9
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-03 12:53:29 +0200

    Frontend: Page-> Admin Login Add Admin Tag Verification

--- fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/login.js
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/login.js
@@ -25,8 +25,15 @@ const AdminLogin = () => {
         password: password,
       })
       .then((res) => {
-        localStorage.setItem("jwt", res.data.token);
-        navigate("/admin/home");
+        // check if email ends with @fallah.com
+        if (email.endsWith("@fallah.com")) {
+          localStorage.setItem("jwt", res.data.token);
+          localStorage.setItem("username", res.data.username);
+          navigate("/admin/home");
+        } else {
+          setError(true);
+          setLoading(false);
+        }
       })
       .catch((err) => {
         console.log(err.message);
