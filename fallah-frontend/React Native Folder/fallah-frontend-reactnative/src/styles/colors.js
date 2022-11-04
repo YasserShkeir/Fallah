2fbff902e12a5db6ee5e9f8c92681bbb64abe0c9
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-04 23:57:18 +0200

    Frontend: Styles-> Color Palette

---
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/styles/colors.js
@@ -0,0 +1,5 @@
+// Set the colors for the app
+export const CREAMWHITE = "#F6F8FF";
+export const LIGHTGREEN = "#23CE6B";
+export const DARKGREEN = "#1E6048";
+export const PEACHYYELLOW = "#F1C24C";
