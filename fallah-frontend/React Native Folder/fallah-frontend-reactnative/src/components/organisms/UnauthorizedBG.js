fc0e011ee0cd7067ff51c6668d758fb694ef81f4
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-05 01:22:04 +0200

    Frontend: Organism-> Add Recent Styling

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/organisms/UnauthorizedBG.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/organisms/UnauthorizedBG.js
@@ -1,24 +1,13 @@
-import { View, ImageBackground } from "react-native";
+import { ImageBackground } from "react-native";
 
 import loginBG from "../../assets/images/loginBG.jpg";
+import { UNAUTHBG } from "../../styles/components";
 
 const UnAuthBackground = ({ children }) => {
   return (
-    <View style={{ backgroundColor: "black" }}>
-      <ImageBackground
-        source={loginBG}
-        style={{
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        {children}
-      </ImageBackground>
-    </View>
+    <ImageBackground source={loginBG} style={UNAUTHBG}>
+      {children}
+    </ImageBackground>
   );
 };
 
