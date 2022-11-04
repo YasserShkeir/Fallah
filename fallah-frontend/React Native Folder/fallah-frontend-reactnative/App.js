8964937a8fd51fe74cbee8fdb097f3d166d0ad89
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-04 13:15:40 +0200

    Frontend: Structure-> React Native Initial Test

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/App.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Step One</Text>
+      <Text>Edit App.js to change this screen and turn it into your app.</Text>
+      <Text style={styles.heading}>See Your Changes</Text>
+      <Text>Press Cmd + R inside the simulator to reload your app’s code.</Text>
+      <Text style={styles.heading}>Debug</Text>
+      <Text>
+        Press Cmd + M or Shake your device to open the React Native Debug Menu.
+      </Text>
+      <Text style={styles.heading}>Learn</Text>
+      <Text>Read the docs to discover what to do next:</Text>
     </View>
   );
 }
@@ -14,8 +22,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
