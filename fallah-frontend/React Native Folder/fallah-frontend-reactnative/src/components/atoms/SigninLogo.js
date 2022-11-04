e2737c12bcb7a14848112b4630e4f6eec66f1493
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-05 00:19:59 +0200

    Frontend: Atom-> Sign In Logo

---
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/atoms/SigninLogo.js
@@ -0,0 +1,20 @@
+import { View, Image } from "react-native";
+
+import loginLogo from "../../assets/images/LightLoginLogo.png";
+
+const SigninLogo = () => {
+  return (
+    <View
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Image source={loginLogo} style={{ width: 300, height: 150 }} />
+    </View>
+  );
+};
+
+export default SigninLogo;
