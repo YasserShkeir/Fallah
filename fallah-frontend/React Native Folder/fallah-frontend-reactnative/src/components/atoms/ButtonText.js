bb4c00da99878ccea0292ddb53632d9b8cf364f9
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-05 01:22:27 +0200

    Frontend: Atom-> Create Button Text Atom

---
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/atoms/ButtonText.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+import { CREAMWHITEBUTTONTEXT } from "../../styles/components";
+
+const ButtonText = ({ text }) => {
+  return <Text style={CREAMWHITEBUTTONTEXT}>{text}</Text>;
+};
+
+export default ButtonText;
