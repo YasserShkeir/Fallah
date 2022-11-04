c47149b427a19ba225fe40c23e76219075eea303
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-04 23:56:52 +0200

    Frontend: Styles-> Component White Text Field

---
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/styles/components.js
@@ -0,0 +1,12 @@
+import { CREAMWHITE } from "./colors";
+
+export const CREAMWHITETEXTFIELD = {
+  width: "80%",
+  height: 40,
+  backgroundColor: CREAMWHITE,
+  borderRadius: 10,
+  alignItems: "center",
+  paddingLeft: 10,
+  paddingRight: 10,
+  marginBottom: 10,
+};
