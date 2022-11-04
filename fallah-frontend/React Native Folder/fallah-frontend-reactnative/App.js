a46830b46d4069e94d4c54e8e53dc0a132c389be
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-04 23:59:09 +0200

    Frontend: App-> Apply Scenes Testing

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/App.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/App.js
@@ -1,15 +1,31 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import { StyleSheet, Text, View } from "react-native";
 
-import Test from "./src/components/atoms/test";
+import SignIn from "./src/scenes/signin/index";
+import SignUp from "./src/scenes/signup/index";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Test />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={SignIn}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
