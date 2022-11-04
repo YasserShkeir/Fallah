c2b63cd6f139a7cf041e09e52bead54f6f09a24a
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-05 01:21:10 +0200

    Frontend: Styles-> Add Styling for Components

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/styles/components.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/styles/components.js
@@ -1,4 +1,4 @@
-import { CREAMWHITE } from "./colors";
+import { CREAMWHITE, LIGHTGREEN, DARKGREEN, PEACHYYELLOW } from "./colors";
 
 export const CREAMWHITETEXTFIELD = {
   width: "80%",
@@ -8,5 +8,33 @@ export const CREAMWHITETEXTFIELD = {
   alignItems: "center",
   paddingLeft: 10,
   paddingRight: 10,
-  marginBottom: 10,
+  marginBottom: 15,
+  marginTop: 10,
+  fontSize: 18,
+};
+
+export const CREAMWHITEBUTTONTEXT = {
+  color: CREAMWHITE,
+  fontSize: 20,
+  fontWeight: "bold",
+};
+
+export const LIGHTGREENBUTTON = {
+  width: "80%",
+  height: 40,
+  backgroundColor: LIGHTGREEN,
+  borderRadius: 10,
+  alignItems: "center",
+  justifyContent: "center",
+  marginBottom: 30,
+  marginTop: 30,
+};
+
+export const UNAUTHBG = {
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
 };
