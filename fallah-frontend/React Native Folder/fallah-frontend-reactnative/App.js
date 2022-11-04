5d8787d6c44d29532ba78481737966737f6b5ac1
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-04 14:10:00 +0200

    Frontend: Structure-> React Native Initial Folder Structure

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/App.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/App.js
@@ -2,19 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import Test from "./src/components/atoms/test";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Step One</Text>
-      <Text>Edit App.js to change this screen and turn it into your app.</Text>
-      <Text style={styles.heading}>See Your Changes</Text>
-      <Text>Press Cmd + R inside the simulator to reload your app’s code.</Text>
-      <Text style={styles.heading}>Debug</Text>
-      <Text>
-        Press Cmd + M or Shake your device to open the React Native Debug Menu.
-      </Text>
-      <Text style={styles.heading}>Learn</Text>
-      <Text>Read the docs to discover what to do next:</Text>
+      <Test />
+      <StatusBar style="auto" />
     </View>
   );
 }
---
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/atoms/test.js
@@ -0,0 +1,21 @@
+import { react } from "react";
+import { View, Text, Image } from "react-native";
+
+import loginLogo from "../../assets/images/LightLoginLogo.png";
+
+const Test = () => {
+  return (
+    <View
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Image source={loginLogo} style={{ width: 300, height: 150 }} />
+    </View>
+  );
+};
+
+export default Test;
