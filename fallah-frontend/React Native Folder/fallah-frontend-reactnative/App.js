61991a9a222ce79604dc71b85646708fa6ba2239
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-06 01:48:20 +0200

    Frontend: App-> Replace deprecated Package

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/App.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Provider as PaperProvider } from "react-native-paper";
-import AppLoading from "expo-app-loading";
-import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
 
 import SignIn from "./src/scenes/signin/index";
 import SignUp from "./src/scenes/signup/index";
@@ -16,24 +16,45 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [token, setToken] = useState(null);
-  const [fontsLoaded] = useFonts({
-    "Inter-ExtraBold": require("./src/assets/fonts/Inter-ExtraBold.ttf"),
-    "Inter-Bold": require("./src/assets/fonts/Inter-Bold.ttf"),
-    "Inter-Medium": require("./src/assets/fonts/Inter-Medium.ttf"),
-    "Inter-Regular": require("./src/assets/fonts/Inter-Regular.ttf"),
-    "Inter-Light": require("./src/assets/fonts/Inter-Light.ttf"),
-  });
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
+  const [appIsReady, setAppIsReady] = useState(false);
 
   const checkToken = async () => {
     const token = await AsyncStorage.getItem("token");
     setToken(token);
   };
 
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          "Inter-ExtraBold": require("./src/assets/fonts/Inter-ExtraBold.ttf"),
+          "Inter-Bold": require("./src/assets/fonts/Inter-Bold.ttf"),
+          "Inter-Medium": require("./src/assets/fonts/Inter-Medium.ttf"),
+          "Inter-Regular": require("./src/assets/fonts/Inter-Regular.ttf"),
+          "Inter-Light": require("./src/assets/fonts/Inter-Light.ttf"),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   checkToken();
+  onLayoutRootView();
 
   return (
     <PaperProvider theme={theme}>
