e74c15a49c4a7b3389540e8830418d85fd25a8f4
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-05 17:44:03 +0200

    Frontend: App-> Add Initial Token Verification

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/App.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/App.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import SignIn from "./src/scenes/signin/index";
 import SignUp from "./src/scenes/signup/index";
+import BuyerLanding from "./src/scenes/buyer/landing";
 import { CREAMWHITE, LIGHTGREEN } from "./src/styles/colors";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [token, setToken] = useState(null);
+
+  const checkToken = async () => {
+    const token = await AsyncStorage.getItem("token");
+    console.log(token);
+    setToken(token);
+  };
+
+  checkToken();
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="Home"
-          component={SignIn}
+          name="SignIn"
+          component={token ? BuyerLanding : SignIn}
           options={{ headerShown: false }}
         />
         <Stack.Screen
@@ -31,6 +43,11 @@ export default function App() {
             // change back arrow color
             headerTintColor: CREAMWHITE,
           }}
+        />
+        <Stack.Screen
+          name="BuyerLanding"
+          component={BuyerLanding}
+          options={{ headerShown: false }}
         />
       </Stack.Navigator>
     </NavigationContainer>
