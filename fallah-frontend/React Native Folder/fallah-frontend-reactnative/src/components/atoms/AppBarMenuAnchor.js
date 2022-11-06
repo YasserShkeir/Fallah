2706530e9009131d979656369323d7022bf77e95
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-06 22:12:17 +0200

    Frontend: Atom-> App Bar Menu Anchor

---
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/atoms/AppBarMenuAnchor.js
@@ -0,0 +1,37 @@
+import { View } from "react-native";
+import { Text, IconButton } from "react-native-paper";
+import { CREAMWHITE } from "../../styles/colors";
+
+const AppBarMenuAnchor = ({ onPress, locations, location }) => {
+  return (
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        paddingLeft: 8,
+        color: CREAMWHITE,
+        alignItems: "center",
+        height: 20,
+      }}
+    >
+      <Text
+        onPress={onPress}
+        style={{
+          fontFamily: "Inter-Bold",
+          color: CREAMWHITE,
+        }}
+        compact={true}
+      >
+        {locations ? location : "Loading..."}
+      </Text>
+      <IconButton
+        icon="chevron-down"
+        iconColor={CREAMWHITE}
+        onPress={onPress}
+        size={20}
+      />
+    </View>
+  );
+};
+
+export default AppBarMenuAnchor;
