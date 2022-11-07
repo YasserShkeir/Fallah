7d19d1532022b5f466e7451f8011a7d5bac062a2
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-07 14:22:59 +0200

    Frontend: Organism-> Buyer App Bar Add Search

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/organisms/BuyerAppBar.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/organisms/BuyerAppBar.js
@@ -1,5 +1,8 @@
 import { View } from "react-native";
-import { Text, Appbar } from "react-native-paper";
+import { Text, Appbar, Searchbar, Button } from "react-native-paper";
+
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 // Components
 import BuyerAppBarLayout from "../molecules/BuyerAppbarLayout";
@@ -7,7 +10,7 @@ import AppbarLocationMenu from "../molecules/BuyerAppbarLocationMenu";
 import AppbarLocationModal from "../molecules/BuyerAppbarLocationModal";
 
 // Styles
-import { CREAMWHITE } from "../../styles/colors";
+import { CREAMWHITE, DARKGREEN } from "../../styles/colors";
 
 const BuyerAppBar = ({ page }) => {
   if (page === "home") {
@@ -27,6 +30,51 @@ const BuyerAppBar = ({ page }) => {
           <AppbarLocationModal />
         </View>
         <Appbar.Action color={CREAMWHITE} icon="bell" onPress={() => {}} />
+      </BuyerAppBarLayout>
+    );
+  }
+
+  // Add other pages here
+  else if (page === "magnify") {
+    return (
+      <BuyerAppBarLayout>
+        <Searchbar
+          icon={() => (
+            <FontAwesome5 name="search" size={20} color={DARKGREEN} />
+          )}
+          placeholder="Search"
+          placeholderTextColor={DARKGREEN}
+          inputStyle={{ fontFamily: "Inter-Bold", color: DARKGREEN }}
+          style={{
+            borderRadius: 10,
+            width: "70%",
+            height: 40,
+          }}
+        ></Searchbar>
+        <Button
+          icon={() => (
+            <MaterialCommunityIcons
+              name="filter-variant"
+              size={26}
+              color={CREAMWHITE}
+            />
+          )}
+          mode="contained"
+          buttonColor={DARKGREEN}
+          onPress={() => console.log("Pressed")}
+          contentStyle={{ flexDirection: "row-reverse" }}
+          labelStyle={{
+            fontFamily: "Inter-Bold",
+            color: CREAMWHITE,
+            fontSize: 16,
+          }}
+          style={{
+            borderRadius: 10,
+            width: "27%",
+          }}
+        >
+          Filter
+        </Button>
       </BuyerAppBarLayout>
     );
   }
