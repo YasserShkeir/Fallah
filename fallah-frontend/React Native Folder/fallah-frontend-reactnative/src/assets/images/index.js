609e48c5a4f0f33ec0c5b61339755b5653c64a0e
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-08 17:34:32 +0200

    Frontend: Assets-> Add Base Category Images

---
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/assets/images/index.js
@@ -0,0 +1,28 @@
+const images = [
+  {
+    name: "Apples",
+    src: require("./apples/allApples.jpg"),
+  },
+  {
+    name: "Bananas",
+    src: require("./bananas/allBananas.jpg"),
+  },
+  {
+    name: "Lettuce",
+    src: require("./lettuce/allLettuce.jpg"),
+  },
+  {
+    name: "Oranges",
+    src: require("./oranges/allOranges.jpg"),
+  },
+  {
+    name: "Potatoes",
+    src: require("./potatoes/allPotatoes.jpg"),
+  },
+  {
+    name: "Onions",
+    src: require("./onions/allOnions.jpg"),
+  },
+];
+
+export default images;
