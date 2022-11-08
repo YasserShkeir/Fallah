b916bb77268173a66ab4713b04e7a7a53d339272
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-08 17:36:18 +0200

    Frontend: Component-> Card Buyer Category Card

---
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/cards/BuyerCategoryCard.js
@@ -0,0 +1,21 @@
+import { Card } from "react-native-paper";
+import { View } from "react-native";
+
+const BuyerCategoryCard = ({ images, category }) => {
+  return (
+    <View style={{ marginBottom: 20 }}>
+      <Card style={{ width: 100, height: 160 }} mode="elevated">
+        <Card.Cover
+          source={
+            images[images.findIndex((image) => image.name === category.catName)]
+              .src
+          }
+          style={{ width: 100, height: 120 }}
+        />
+        <Card.Title title={category.catName} />
+      </Card>
+    </View>
+  );
+};
+
+export default BuyerCategoryCard;
