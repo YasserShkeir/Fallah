3a1f4249f876bb14347a2f46c3a727a91288719b
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-08 17:36:38 +0200

    Frontend: Component-> Section Buyer Categories Section

---
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/sections/BuyerCategoriesSection.js
@@ -0,0 +1,55 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+import BuyerCategoryCard from "../cards/BuyerCategoryCard";
+import { LIGHTGREEN, DARKGREEN } from "../../styles/colors";
+
+const BuyerCategoriesSection = (categories) => {
+  // test
+  console.log("categories", categories);
+  console.log("images", categories.images);
+  return (
+    <View
+      style={{
+        borderBottomColor: LIGHTGREEN,
+        borderBottomWidth: 2,
+        paddingHorizontal: 12,
+      }}
+    >
+      <Text
+        style={{
+          marginBottom: 10,
+          fontFamily: "Inter-Bold",
+          fontSize: 22,
+          color: DARKGREEN,
+        }}
+      >
+        Top Sought Categories
+      </Text>
+      <View
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          flexWrap: "wrap",
+        }}
+      >
+        {categories ? (
+          categories.categories.slice(0, 6).map((category) => {
+            return (
+              <BuyerCategoryCard
+                key={category.childCategory._id}
+                images={categories.images}
+                category={category}
+              />
+            );
+          })
+        ) : (
+          <Text>Loading...</Text>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default BuyerCategoriesSection;
