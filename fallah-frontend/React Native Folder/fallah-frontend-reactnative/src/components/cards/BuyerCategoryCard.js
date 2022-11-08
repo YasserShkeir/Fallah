75c1f37bdffa7a0db94d7a448c6ede1e086f2c20
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-08 22:34:17 +0200

    Frontend: Component-> Card Buyer Category Card Style Edits

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/cards/BuyerCategoryCard.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/cards/BuyerCategoryCard.js
@@ -1,20 +1,39 @@
 import { Card } from "react-native-paper";
 import { View } from "react-native";
+import { CREAMWHITE, LIGHTGREEN } from "../../styles/colors";
 
 const BuyerCategoryCard = ({ images, category }) => {
   return (
-    <View style={{ marginBottom: 20 }}>
-      <Card style={{ width: 100, height: 160 }} mode="elevated">
+    <View
+      style={{
+        marginBottom: 20,
+        borderColor: LIGHTGREEN,
+        borderWidth: 4,
+        borderRadius: 8,
+      }}
+    >
+      <Card style={{ width: 100, height: 150 }} mode="elevated" elevation={1}>
         <Card.Cover
           source={
             images[images.findIndex((image) => image.name === category.catName)]
               .src
           }
-          style={{ width: 100, height: 120 }}
+          style={{ width: 100, height: 120, backgroundColor: CREAMWHITE }}
         />
         <Card.Title
           title={category.catName}
-          titleStyle={{ fontFamily: "Inter-Regular", fontSize: 14 }}
+          titleStyle={{
+            fontFamily: "Inter-Medium",
+            fontSize: 14,
+            color: LIGHTGREEN,
+            textAlignVertical: "center",
+          }}
+          style={{
+            backgroundColor: CREAMWHITE,
+            height: 30,
+            minHeight: 30,
+            borderRadius: 8,
+          }}
         />
       </Card>
     </View>
