a31f8cbc46366f18333373bb4d60a1eebd92dfab
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-08 17:35:45 +0200

    Frontend: Hook-> Users Get Categories

---
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/hooks/getCategories.js
@@ -0,0 +1,22 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { LOCALIP } from "@env";
+
+export const getCategories = async (func) => {
+  const url = `${LOCALIP}/users/category`;
+  const token = await AsyncStorage.getItem("token");
+
+  try {
+    await axios
+      .get(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        func(response);
+      });
+  } catch (error) {
+    console.error(error);
+  }
+};
