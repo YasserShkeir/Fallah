f085ade9bbd34dc88bc501de803455e41b0a995b
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-09 19:31:54 +0200

    Frontend: Scene-> Buyer Search Route Add Hooks + Components

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/scenes/buyer/BuyerSearchRoute.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/scenes/buyer/BuyerSearchRoute.js
@@ -1,16 +1,123 @@
-import BuyerMainLayout from "../../components/layouts/BuyerMainLayout";
-import BuyerAppBar from "../../components/appbars/BuyerAppBar";
-import { ScrollView } from "react-native";
+import { useState, useEffect } from "react";
+import { ScrollView, View } from "react-native";
 import { Button, Text } from "react-native-paper";
 
+// Components
+import BuyerMainLayout from "../../components/layouts/BuyerMainLayout";
+import BuyerAppBar from "../../components/appbars/BuyerAppBar";
+import BuyerSearchCard from "../../components/cards/BuyerSearchCard";
+
+// Hooks
+import { getSearchProducts } from "../../hooks/buyerCategories";
+import { getFarmers } from "../../hooks/buyerFarmer";
+
+// Style
+import { DARKGREEN, LIGHTGREEN } from "../../styles/colors";
+
 const BuyerSearchRoute = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [farmers, setFarmers] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [value, setValue] = useState("Farmers");
+
+  const onChangeSearch = (query) => setSearchQuery(query);
+
+  const getFarmersHandler = (response) => {
+    setFarmers(response.data.farmers);
+  };
+
+  const getProductsHandler = (response) => {
+    setProducts(response.data.products);
+  };
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await getFarmers(getFarmersHandler);
+        await getSearchProducts(getProductsHandler);
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    prepare();
+  }, []);
+
   return (
     <BuyerMainLayout>
-      <BuyerAppBar page="search" />
+      <BuyerAppBar page="search" prop={onChangeSearch} />
       <ScrollView style={{ paddingHorizontal: 10, paddingVertical: 5 }}>
-        <Button style={{ width: "100%", backgroundColor: "black" }}>
-          <Text>Test</Text>
-        </Button>
+        <View>
+          <View
+            style={{ display: "flex", flexDirection: "row", width: "100%" }}
+          >
+            <Button
+              onPress={() => {
+                setValue("Farmers");
+              }}
+              style={{
+                borderRadius: 0,
+                width: "50%",
+                ...(value === "Farmers"
+                  ? { borderBottomWidth: 2, borderBottomColor: LIGHTGREEN }
+                  : {}),
+              }}
+            >
+              <Text
+                style={{
+                  fontFamily: "Inter-Bold",
+                  color: value === "Farmers" ? LIGHTGREEN : DARKGREEN,
+                  fontSize: 16,
+                }}
+              >
+                Farmers
+              </Text>
+            </Button>
+            <Button
+              onPress={() => {
+                setValue("Products");
+              }}
+              style={{
+                borderRadius: 0,
+                width: "50%",
+                ...(value === "Products"
+                  ? { borderBottomWidth: 2, borderBottomColor: LIGHTGREEN }
+                  : {}),
+              }}
+            >
+              <Text
+                style={{
+                  fontFamily: "Inter-Bold",
+                  color: value === "Products" ? LIGHTGREEN : DARKGREEN,
+                  fontSize: 16,
+                }}
+              >
+                Products
+              </Text>
+            </Button>
+          </View>
+
+          {value === "Farmers"
+            ? farmers.slice(0, 5).map((farmer) => {
+                if (searchQuery === "") {
+                  return <BuyerSearchCard key={farmer._id} item={farmer} />;
+                } else if (
+                  farmer.name.toLowerCase().includes(searchQuery.toLowerCase())
+                ) {
+                  return <BuyerSearchCard key={farmer._id} item={farmer} />;
+                }
+              })
+            : products.map((product) => {
+                if (searchQuery === "") {
+                  return <BuyerSearchCard key={product._id} item={product} />;
+                } else if (
+                  product.productName
+                    .toLowerCase()
+                    .includes(searchQuery.toLowerCase())
+                ) {
+                  return <BuyerSearchCard key={product._id} item={product} />;
+                }
+              })}
+        </View>
       </ScrollView>
     </BuyerMainLayout>
   );
