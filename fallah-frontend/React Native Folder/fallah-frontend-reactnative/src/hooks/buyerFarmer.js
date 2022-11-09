021abd914cb6037ccf61b784b7b0f96213ca3d36
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-09 19:30:00 +0200

    Frontend: Hook-> Buyer Get Farmers + Get Followed Farmers

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/hooks/buyerFarmer.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/hooks/buyerFarmer.js
@@ -2,8 +2,27 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import { LOCALIP } from "@env";
 
+export const getFarmers = async (func, id1) => {
+  const url = `${LOCALIP}/users/farmer${id1 ? `/${id1}` : ""}`;
+  const token = await AsyncStorage.getItem("token");
+
+  try {
+    await axios
+      .get(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        func(response);
+      });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export const buyerGetFavourites = async (func) => {
-  const url = `${LOCALIP}/users/follow`;
+  const url = `${LOCALIP}/users/farmer/following`;
   const token = await AsyncStorage.getItem("token");
 
   try {
