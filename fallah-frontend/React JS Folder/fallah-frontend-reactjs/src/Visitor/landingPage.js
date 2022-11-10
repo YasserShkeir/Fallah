29b1ab67bbb0fc5a8bee098280caad7d3170e9e1
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-10 03:14:30 +0200

    Frontend: Visitor-> Footer Version 2 (Commented 1)

--- fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Visitor/landingPage.js
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Visitor/landingPage.js
@@ -14,6 +14,19 @@ import AllProductsBottom from "./assets/images/AllProductsBottom2.png";
 import FooterImageTop from "./assets/images/visitorFooterTop.png";
 import FooterImageBot from "./assets/images/visitorFooter.png";
 
+import Footer2ImageRight from "./assets/footerV2images/SideImages/1.png";
+import Footer2ImageLeft1 from "./assets/footerV2images/SideImages/2.png";
+import Footer2ImageLeft2 from "./assets/footerV2images/SideImages/3.png";
+
+import Fruit1 from "./assets/footerV2images/Fruits/1.png";
+import Fruit2 from "./assets/footerV2images/Fruits/2.png";
+import Fruit3 from "./assets/footerV2images/Fruits/3.png";
+import Fruit4 from "./assets/footerV2images/Fruits/4.png";
+import Fruit5 from "./assets/footerV2images/Fruits/5.png";
+import Fruit6 from "./assets/footerV2images/Fruits/6.png";
+import Fruit7 from "./assets/footerV2images/Fruits/7.png";
+import Fruit8 from "./assets/footerV2images/Fruits/8.png";
+
 const AllProducts = () => {
   // Text on left and image on right
   return (
@@ -93,9 +106,50 @@ const Benefits = () => {
 const Footer = () => {
   return (
     <Box className="w-full">
-      <img src={FooterImageTop} className="w-full" />
+      {/* Version 1 of Footer */}
+      {/* <img src={FooterImageTop} className="w-full" />
       <Box className="w-full bg-light-green">
         <img src={FooterImageBot} className="w-full -mt-8" />
+      </Box> */}
+      {/* Version 2 of Footer */}
+      <img src={FooterImageTop} className="w-full" />
+      <Box className="w-full h-fit flex bg-light-green justify-between -mt-3">
+        {/* Left Side */}
+        <Box className="flex flex-col">
+          <img src={Footer2ImageLeft1} className="w-24 sm:w-52" />
+          <img
+            src={Footer2ImageLeft2}
+            className="w-24 sm:w-52 -mt-3 sm:-mt-10 -ml-3 sm:-ml-7"
+          />
+        </Box>
+        {/* Middle Side */}
+        <Box className="h-auto flex flex-col justify-between font-bold items-center text-sm sm:text-3xl text-cream-white pb-8 gap-5">
+          <p>Fresh from the Farmer...</p>
+          <Box className="flex h-fit flex-wrap gap-4 align-middle justify-center">
+            <Box className="flex h-fit gap-4 align-middle justify-center">
+              <img src={Fruit1} className="w-8 sm:w-24" />
+              <img src={Fruit2} className="w-8 sm:w-24" />
+            </Box>
+            <Box className="flex h-fit gap-4 align-middle justify-center">
+              <img src={Fruit3} className="w-8 sm:w-24" />
+              <img src={Fruit4} className="w-8 sm:w-24" />
+            </Box>
+            <Box className="flex h-fit gap-4 align-middle justify-center">
+              <img src={Fruit5} className="w-8 sm:w-24" />
+              <img src={Fruit6} className="w-8 sm:w-24" />
+            </Box>
+            <Box className="flex h-fit gap-4 align-middle justify-center">
+              <img src={Fruit7} className="w-8 sm:w-24" />
+              <img src={Fruit8} className="w-8 sm:w-24" />
+            </Box>
+          </Box>
+
+          <p>... Fresh to the Buyer</p>
+        </Box>
+        {/* Right Side */}
+        <Box>
+          <img src={Footer2ImageRight} className="w-24 sm:w-52" />
+        </Box>
       </Box>
     </Box>
   );
