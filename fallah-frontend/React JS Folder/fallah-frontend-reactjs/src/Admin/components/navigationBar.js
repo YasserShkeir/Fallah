015ed266ff4affa4074763b3784d222913b3dc43
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-13 01:55:02 +0200

    Frontend: Admin-> Component NavBar Add New Link + Responsiveness

--- fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/components/navigationBar.js
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/components/navigationBar.js
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom";
 import { useState } from "react";
+import NavBarLink from "./NavBarLink";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
   faUsers,
@@ -17,27 +16,14 @@ import { Button } from "@mui/material";
 const AdminNavigationBar = () => {
   const [url, setUrl] = useState(window.location.pathname);
 
-  const NavBarLink = ({ to, children, icon, curr }) => {
-    return (
-      <Link
-        to={to}
-        className={
-          "flex items-center text-cream-white text-sm md:text-base font-bold px-4 gap-4 sm:gap-6 bg-dark-green rounded-md w-full " +
-          (curr === to ? " border-4 py-1 border-cream-white" : " py-2")
-        }
-        hover={{ className: "bg-light-green" }}
-        onClick={() => setUrl(to)}
-      >
-        <FontAwesomeIcon icon={icon} className="justify-self-start" />{" "}
-        {children}
-      </Link>
-    );
-  };
-
   return (
-    <div className="bg-light-green flex flex-col sm:w-48 md:w-56 items-center px-5 h-screen justify-between pb-5 fixed overflow-y-auto">
-      <div className="w-full flex flex-col gap-2 sm:gap-3 items-center">
-        <img src={logo} alt="logo" className="w-20 sm:w-32 md:w-42" />
+    <div className="bg-light-green flex flex-col w-20 sm:w-48 md:w-56 items-center px-2 h-screen justify-between pb-5 fixed overflow-y-auto">
+      <div className="w-full flex flex-col gap-2 sm:gap-3 items-center mt-10 sm:mt-0">
+        <img
+          src={logo}
+          alt="logo"
+          className="hidden sm:block sm:w-32 md:w-42"
+        />
         {[
           { to: "/admin/home", icon: faHome, text: "Home" },
           { to: "/admin/users", icon: faUsers, text: "Users" },
@@ -46,7 +32,13 @@ const AdminNavigationBar = () => {
           { to: "/admin/categories", icon: faTree, text: "Categories" },
           { to: "/admin/reviews", icon: faStar, text: "Reviews" },
         ].map((item) => (
-          <NavBarLink to={item.to} icon={item.icon} curr={url} key={item.to}>
+          <NavBarLink
+            to={item.to}
+            icon={item.icon}
+            curr={url}
+            key={item.to}
+            setUrl={setUrl}
+          >
             {item.text}
           </NavBarLink>
         ))}
@@ -54,7 +46,7 @@ const AdminNavigationBar = () => {
       <Button
         variant="contained"
         color="creamWhite"
-        className="w-full rounded-3xl hover:bg-dark-green hover:text-cream-white"
+        className="w-16 sm:w-full rounded-3xl hover:bg-dark-green hover:text-cream-white"
         style={{ marginTop: "40px" }}
         onClick={() => {
           localStorage.clear();
