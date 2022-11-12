6477a4c8100f9852ec8825547a260df105f7f3c2
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-12 03:21:38 +0200

    Frontend: Admin-> Add Buyer Chart

--- fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/usersList.js
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/usersList.js
@@ -16,6 +16,7 @@ const sdk = new ChartsEmbedSDK({
 
 const AdminUsersPage = () => {
   const [farmerSearch, setFarmerSearch] = useState("");
+  const [buyerSearch, setBuyerSearch] = useState("");
 
   const farmersChart = sdk.createChart({
     chartId: "636e60b3-ef5e-47da-8e0c-bf0dc8ea2295",
@@ -30,9 +31,26 @@ const AdminUsersPage = () => {
       : {},
   });
 
+  const buyersChart = sdk.createChart({
+    chartId: "a4b5017c-7aa9-4974-9ee7-8b81af8ccaf7",
+    // IF buyerSearch is entered in the search bar, filter the chart by the search term
+    filter: buyerSearch
+      ? {
+          $or: [
+            { name: { $regex: buyerSearch, $options: "i" } },
+            { email: { $regex: buyerSearch, $options: "i" } },
+          ],
+        }
+      : {},
+  });
+
   useEffect(() => {
     farmersChart.render(document.getElementById("farmers-dashboard"));
   }, [farmersChart]);
+
+  useEffect(() => {
+    buyersChart.render(document.getElementById("buyers-dashboard"));
+  }, [buyersChart]);
 
   return (
     <AdminLayout>
@@ -58,6 +76,30 @@ const AdminUsersPage = () => {
           </Box>
           <Box
             id="farmers-dashboard"
+            className="h-full min-w-80 flex-grow"
+          ></Box>
+        </Box>
+        <Box id="buyers" className="h-1/2 w-auto flex gap-3">
+          <Box
+            className="flex flex-col h-full w-64 px-4 py-3 gap-2"
+            style={{ backgroundColor: "white" }}
+          >
+            <Typography variant="h6" className="text-dark-green font-bold">
+              Buyers Controller
+            </Typography>
+            <TextField
+              label="Search Buyer"
+              variant="outlined"
+              size="small"
+              className="w-full my-2"
+              value={buyerSearch}
+              onChange={(e) => {
+                setBuyerSearch(e.target.value);
+              }}
+            />
+          </Box>
+          <Box
+            id="buyers-dashboard"
             className="h-full min-w-80 flex-grow"
           ></Box>
         </Box>
