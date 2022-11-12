bcfd1447b3444490256ab4a43b8ba6ed113ce0e0
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-13 01:57:48 +0200

    Frontend: Admin-> Page Users List Implement Edits + Responsiveness

--- fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/usersList.js
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/usersList.js
@@ -2,9 +2,9 @@ import { React, useState, useEffect } from "react";
 import ChartsEmbedSDK from "@mongodb-js/charts-embed-dom";
 
 import { Box } from "@mui/system";
-import { TextField, Typography } from "@mui/material";
 
-import AdminLayout from "../components/bodyLayout";
+import AdminLayout from "../components/AdminBodyLayout";
+import AdminUserDataControl from "../components/AdminUserDataControl";
 
 const sdk = new ChartsEmbedSDK({
   baseUrl: "https://charts.mongodb.com/charts-fallah-jbobq",
@@ -17,15 +17,20 @@ const sdk = new ChartsEmbedSDK({
 const AdminUsersPage = () => {
   const [farmerSearch, setFarmerSearch] = useState("");
   const [buyerSearch, setBuyerSearch] = useState("");
+  const [deleteFarmerID, setDeleteFarmerID] = useState("");
+  const [deleteBuyerID, setDeleteBuyerID] = useState("");
 
   const farmersChart = sdk.createChart({
     chartId: "636e60b3-ef5e-47da-8e0c-bf0dc8ea2295",
-    // IF farmerSearch is entered in the search bar, filter the chart by the search term
+    autoRefresh: true,
+    maxDataAge: 20,
+    background: "transparent",
     filter: farmerSearch
       ? {
           $or: [
             { name: { $regex: farmerSearch, $options: "i" } },
             { email: { $regex: farmerSearch, $options: "i" } },
+            { phone: { $regex: farmerSearch, $options: "i" } },
           ],
         }
       : {},
@@ -33,12 +38,15 @@ const AdminUsersPage = () => {
 
   const buyersChart = sdk.createChart({
     chartId: "a4b5017c-7aa9-4974-9ee7-8b81af8ccaf7",
-    // IF buyerSearch is entered in the search bar, filter the chart by the search term
+    autoRefresh: true,
+    maxDataAge: 20,
+    background: "transparent",
     filter: buyerSearch
       ? {
           $or: [
             { name: { $regex: buyerSearch, $options: "i" } },
             { email: { $regex: buyerSearch, $options: "i" } },
+            { phone: { $regex: buyerSearch, $options: "i" } },
           ],
         }
       : {},
@@ -46,63 +54,29 @@ const AdminUsersPage = () => {
 
   useEffect(() => {
     farmersChart.render(document.getElementById("farmers-dashboard"));
-  }, [farmersChart]);
-
-  useEffect(() => {
     buyersChart.render(document.getElementById("buyers-dashboard"));
-  }, [buyersChart]);
+  }, [farmersChart, buyersChart]);
 
   return (
     <AdminLayout>
-      <Box className="flex flex-col h-full w-auto my-8 gap-3">
-        <Box id="farmers" className="h-1/2 w-auto flex gap-3">
-          <Box
-            className="flex flex-col h-full w-64 px-4 py-3 gap-2"
-            style={{ backgroundColor: "white" }}
-          >
-            <Typography variant="h6" className="text-dark-green font-bold">
-              Farmers Controller
-            </Typography>
-            <TextField
-              label="Search Farmer"
-              variant="outlined"
-              size="small"
-              className="w-full my-2"
-              value={farmerSearch}
-              onChange={(e) => {
-                setFarmerSearch(e.target.value);
-              }}
-            />
-          </Box>
-          <Box
-            id="farmers-dashboard"
-            className="h-full min-w-80 flex-grow"
-          ></Box>
-        </Box>
-        <Box id="buyers" className="h-1/2 w-auto flex gap-3">
-          <Box
-            className="flex flex-col h-full w-64 px-4 py-3 gap-2"
-            style={{ backgroundColor: "white" }}
-          >
-            <Typography variant="h6" className="text-dark-green font-bold">
-              Buyers Controller
-            </Typography>
-            <TextField
-              label="Search Buyer"
-              variant="outlined"
-              size="small"
-              className="w-full my-2"
-              value={buyerSearch}
-              onChange={(e) => {
-                setBuyerSearch(e.target.value);
-              }}
-            />
-          </Box>
-          <Box
-            id="buyers-dashboard"
-            className="h-full min-w-80 flex-grow"
-          ></Box>
-        </Box>
+      <Box className="flex flex-col w-auto gap-3">
+        <p className="font-bold text-lg">Users List</p>
+        <AdminUserDataControl
+          id="farmers"
+          name="Farmers"
+          searchValue={farmerSearch}
+          search={setFarmerSearch}
+          deleteValue={deleteFarmerID}
+          delete={setDeleteFarmerID}
+        />
+        <AdminUserDataControl
+          id="buyers"
+          name="Buyers"
+          searchValue={buyerSearch}
+          search={setBuyerSearch}
+          deleteValue={deleteBuyerID}
+          delete={setDeleteBuyerID}
+        />
       </Box>
     </AdminLayout>
   );
