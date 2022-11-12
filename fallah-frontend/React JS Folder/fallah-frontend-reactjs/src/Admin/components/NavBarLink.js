a455ea1e7e036df6caada91be22d74429249bfa0
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-13 01:54:28 +0200

    Frontend: Admin-> Component NavBar link Edit Responsiveness

---
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/components/NavBarLink.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const NavBarLink = ({ to, children, icon, curr, setUrl }) => {
+  return (
+    <Link
+      to={to}
+      className={
+        "flex items-center text-cream-white text-sm md:text-base font-bold px-3 sm:px-4 gap-4 sm:gap-6 bg-dark-green rounded-md w-fit sm:w-full " +
+        (curr === to ? " py-2 bg-cream-white text-dark-green" : " py-2")
+      }
+      onClick={() => setUrl(to)}
+    >
+      <FontAwesomeIcon
+        icon={icon}
+        size="1x"
+        className={curr === to ? "text-dark-green justify-self-start" : ""}
+      />{" "}
+      <p
+        className={
+          curr === to
+            ? "text-dark-green justify-self-start hidden sm:block"
+            : "hidden sm:block"
+        }
+      >
+        {children}
+      </p>
+    </Link>
+  );
+};
+
+export default NavBarLink;
