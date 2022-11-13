0cfce34dd922943d01e37079f809fbb5deb81292
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-13 04:10:16 +0200

    Frontend: Admin-> Page Orders List Add Charts

--- fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/ordersList.js
+++ fallah-frontend/React JS Folder/fallah-frontend-reactjs/src/Admin/pages/ordersList.js
@@ -1,13 +1,47 @@
-import { React } from "react";
+import { React, useEffect } from "react";
+import { Box } from "@mui/material";
+import ChartsEmbedSDK from "@mongodb-js/charts-embed-dom";
+import AdminLayout from "../components/AdminBodyLayout";
 
-import AdminNavigationBar from "../components/navigationBar";
+const sdk = new ChartsEmbedSDK({
+  baseUrl: "https://charts.mongodb.com/charts-fallah-jbobq",
+  getUserToken: async () => {
+    const token = localStorage.getItem("jwt");
+    return token;
+  },
+});
 
 const AdminOrdersPage = () => {
+  const regularOrdersChart = sdk.createChart({
+    chartId: "63703f0c-02c9-4dea-8185-f6da73666e64",
+    autoRefresh: true,
+    maxDataAge: 60,
+    background: "transparent",
+  });
+
+  const scheduledOrdersChart = sdk.createChart({
+    chartId: "6370509b-e681-4189-813e-94288a741420",
+    autoRefresh: true,
+    maxDataAge: 60,
+    background: "transparent",
+  });
+
+  useEffect(() => {
+    regularOrdersChart.render(document.getElementById("regularOrdersChart"));
+    scheduledOrdersChart.render(
+      document.getElementById("scheduledOrdersChart")
+    );
+  }, [regularOrdersChart, scheduledOrdersChart]);
+
   return (
-    <div className="flex">
-      <AdminNavigationBar />
-      <h1>AdminOrdersPagee</h1>
-    </div>
+    <AdminLayout>
+      <p className="font-bold text-lg">Orders List</p>
+      <Box id="regularOrdersChart" className="h-full min-w-80 flex-grow"></Box>
+      <Box
+        id="scheduledOrdersChart"
+        className="h-full min-w-80 flex-grow"
+      ></Box>
+    </AdminLayout>
   );
 };
 
