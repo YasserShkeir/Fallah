0b26747911763b882bd163cbe760b91fe1bd6faf
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-14 03:52:33 +0200

    Frontend: Buyer-> Scene Farmer Profile Initial Implementation

---
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/scenes/buyer/BuyerFarmerProfile.js
@@ -0,0 +1,86 @@
+import { useState, useEffect } from "react";
+import { View, Image } from "react-native";
+import { Button, IconButton, Text } from "react-native-paper";
+import IonIcons from "react-native-vector-icons/Ionicons";
+
+import { CREAMWHITE, LIGHTGREEN } from "../../styles/colors";
+
+import { getFollowing } from "../../hooks/buyerFarmer";
+
+const BuyerFarmerProfile = ({ route }) => {
+  // Access data sent from payload
+  const { farmer } = route.params;
+
+  const [following, setFollowing] = useState([]);
+  const [followed, setFollowed] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const getFollowingHandler = (response) => {
+    response.data.following.forEach((follow) => {
+      if (follow._id === farmer._id) {
+        setFollowed(true);
+      }
+    });
+    setFollowing(response.data.following);
+  };
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await getFollowing(getFollowingHandler);
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    prepare();
+  }, []);
+
+  return (
+    <View>
+      <View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-end",
+            alignItems: "center",
+            width: "100%",
+            padding: 10,
+            position: "absolute",
+            top: 0,
+            zIndex: 1,
+          }}
+        >
+          <IconButton
+            // Check if the user is already following the farmer
+            icon={followed ? "heart" : "heart-outline"}
+            disabled={loading}
+            iconColor={LIGHTGREEN}
+            size={25}
+            style={{
+              backgroundColor: CREAMWHITE,
+              borderRadius: 10,
+            }}
+            onPress={() => {
+              setFollowed(!followed);
+            }}
+          />
+        </View>
+        <Image
+          source={{ uri: farmer.images[0] }}
+          style={{ width: "100%", height: 250 }}
+        />
+      </View>
+
+      <View style={{ padding: 10 }}>
+        <Text
+          style={{ fontFamily: "Inter-Bold", fontSize: 24, color: LIGHTGREEN }}
+        >
+          {farmer.name}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default BuyerFarmerProfile;
