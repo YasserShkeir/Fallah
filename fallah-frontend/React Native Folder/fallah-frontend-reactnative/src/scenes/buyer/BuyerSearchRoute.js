50d60f4cd2ba51b8b179df5548afd382cc88213d
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-15 15:56:05 +0200

    Frontend: Buyer-> Scene Search Add Search Type Bar

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/scenes/buyer/BuyerSearchRoute.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/scenes/buyer/BuyerSearchRoute.js
@@ -1,18 +1,15 @@
 import { useState, useEffect } from "react";
-import { ScrollView, View } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { ScrollView } from "react-native";
 
 // Components
 import BuyerMainLayout from "../../components/layouts/BuyerMainLayout";
 import BuyerAppBar from "../../components/appbars/BuyerAppBar";
 import BuyerSearchCard from "../../components/cards/BuyerSearchCard";
+import BuyerSearchType from "../../components/sections/Search/BuyerSearchType";
 
 // Hooks
 import { getSearchProducts } from "../../hooks/buyerCategories";
 import { getFarmers } from "../../hooks/buyerFarmer";
-
-// Style
-import { DARKGREEN, LIGHTGREEN } from "../../styles/colors";
 
 const BuyerSearchRoute = ({ navigation }) => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -45,103 +42,53 @@ const BuyerSearchRoute = ({ navigation }) => {
   return (
     <BuyerMainLayout>
       <BuyerAppBar page="search" prop={onChangeSearch} />
+      <BuyerSearchType value={value} setValue={setValue} />
       <ScrollView style={{ paddingHorizontal: 10, paddingVertical: 5 }}>
-        <View>
-          <View
-            style={{ display: "flex", flexDirection: "row", width: "100%" }}
-          >
-            <Button
-              onPress={() => {
-                setValue("Farmers");
-              }}
-              style={{
-                borderRadius: 0,
-                width: "50%",
-                ...(value === "Farmers"
-                  ? { borderBottomWidth: 2, borderBottomColor: LIGHTGREEN }
-                  : {}),
-              }}
-            >
-              <Text
-                style={{
-                  fontFamily: "Inter-Bold",
-                  color: value === "Farmers" ? LIGHTGREEN : DARKGREEN,
-                  fontSize: 16,
-                }}
-              >
-                Farmers
-              </Text>
-            </Button>
-            <Button
-              onPress={() => {
-                setValue("Products");
-              }}
-              style={{
-                borderRadius: 0,
-                width: "50%",
-                ...(value === "Products"
-                  ? { borderBottomWidth: 2, borderBottomColor: LIGHTGREEN }
-                  : {}),
-              }}
-            >
-              <Text
-                style={{
-                  fontFamily: "Inter-Bold",
-                  color: value === "Products" ? LIGHTGREEN : DARKGREEN,
-                  fontSize: 16,
-                }}
-              >
-                Products
-              </Text>
-            </Button>
-          </View>
-
-          {value === "Farmers"
-            ? farmers.slice(0, 5).map((farmer) => {
-                if (searchQuery === "") {
-                  return (
-                    <BuyerSearchCard
-                      key={farmer._id}
-                      item={farmer}
-                      navigation={navigation}
-                    />
-                  );
-                } else if (
-                  farmer.name.toLowerCase().includes(searchQuery.toLowerCase())
-                ) {
-                  return (
-                    <BuyerSearchCard
-                      key={farmer._id}
-                      item={farmer}
-                      navigation={navigation}
-                    />
-                  );
-                }
-              })
-            : products.map((product) => {
-                if (searchQuery === "") {
-                  return (
-                    <BuyerSearchCard
-                      key={product._id}
-                      item={product}
-                      navigation={navigation}
-                    />
-                  );
-                } else if (
-                  product.productName
-                    .toLowerCase()
-                    .includes(searchQuery.toLowerCase())
-                ) {
-                  return (
-                    <BuyerSearchCard
-                      key={product._id}
-                      item={product}
-                      navigation={navigation}
-                    />
-                  );
-                }
-              })}
-        </View>
+        {value === "Farmers"
+          ? farmers.slice(0, 5).map((farmer) => {
+              if (searchQuery === "") {
+                return (
+                  <BuyerSearchCard
+                    key={farmer._id}
+                    item={farmer}
+                    navigation={navigation}
+                  />
+                );
+              } else if (
+                farmer.name.toLowerCase().includes(searchQuery.toLowerCase())
+              ) {
+                return (
+                  <BuyerSearchCard
+                    key={farmer._id}
+                    item={farmer}
+                    navigation={navigation}
+                  />
+                );
+              }
+            })
+          : products.map((product) => {
+              if (searchQuery === "") {
+                return (
+                  <BuyerSearchCard
+                    key={product._id}
+                    item={product}
+                    navigation={navigation}
+                  />
+                );
+              } else if (
+                product.productName
+                  .toLowerCase()
+                  .includes(searchQuery.toLowerCase())
+              ) {
+                return (
+                  <BuyerSearchCard
+                    key={product._id}
+                    item={product}
+                    navigation={navigation}
+                  />
+                );
+              }
+            })}
       </ScrollView>
     </BuyerMainLayout>
   );
