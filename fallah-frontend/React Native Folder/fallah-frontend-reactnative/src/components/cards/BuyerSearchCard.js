cd04a0171635a864d4c143689cb485615c02dc1c
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-16 03:50:12 +0200

    Frontend: Buyer-> Component Card Search Add BuyerItemCard

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/cards/BuyerSearchCard.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/cards/BuyerSearchCard.js
@@ -1,10 +1,14 @@
 import { TouchableOpacity, View, Image } from "react-native";
 import { Text } from "react-native-paper";
 
-// Styles
-import { LIGHTGREEN, CREAMWHITE } from "../../styles/colors";
+// Components
+import BuyerItemCard from "./BuyerItemCard";
 
-const BuyerSearchCard = ({ item, navigation }) => {
+// Styles
+import { LIGHTGREEN, DARKGREEN, CREAMWHITE } from "../../styles/colors";
+
+const BuyerSearchCard = ({ item, location, navigation }) => {
+  console.log(item);
   const image = item.images[0];
   if (item.__t === "Farmer") {
     return (
@@ -42,25 +46,37 @@ const BuyerSearchCard = ({ item, navigation }) => {
             flexDirection: "column",
             justifyContent: "space-between",
             height: 100,
+            width: "60%",
+            paddingLeft: 10,
+            fontFamily: "Inter-Bold",
+            color: CREAMWHITE,
           }}
         >
           <Text
             style={{
               width: 200,
-              paddingLeft: 10,
               fontFamily: "Inter-Bold",
-              fontSize: 20,
               color: CREAMWHITE,
+              fontSize: 20,
             }}
           >
             {item.name}
           </Text>
           <Text
             style={{
-              paddingLeft: 10,
-              fontFamily: "Inter-Bold",
               fontSize: 14,
+              fontFamily: "Inter-Medium",
               color: CREAMWHITE,
+            }}
+            numberOfLines={2}
+          >
+            {item.description}
+          </Text>
+          <Text
+            style={{
+              fontSize: 14,
+              fontFamily: "Inter-Bold",
+              color: DARKGREEN,
             }}
           >
             User Since {item.created_at.slice(0, 10)}
@@ -71,67 +87,12 @@ const BuyerSearchCard = ({ item, navigation }) => {
     );
   } else {
     return (
-      <TouchableOpacity
-        onPress={() =>
-          navigation.navigate("BuyerProduct", {
-            product: item,
-          })
-        }
-        style={{
-          width: "100%",
-          height: 120,
-          marginHorizontal: 3,
-          marginVertical: 5,
-          backgroundColor: LIGHTGREEN,
-          justifyContent: "flex-start",
-          alignItems: "center",
-          borderRadius: 10,
-          display: "flex",
-          flexDirection: "row",
-          paddingHorizontal: 10,
-        }}
-      >
-        <Image
-          source={{ uri: image }}
-          style={{
-            width: "40%",
-            height: 100,
-            borderRadius: 10,
-          }}
-        />
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            height: 100,
-          }}
-        >
-          <Text
-            style={{
-              height: 50,
-              width: 200,
-              paddingLeft: 10,
-              fontFamily: "Inter-Bold",
-              fontSize: 16,
-              color: CREAMWHITE,
-            }}
-          >
-            {item.productName}
-          </Text>
-          <Text
-            style={{
-              textAlignVertical: "center",
-              paddingLeft: 10,
-              fontFamily: "Inter-Bold",
-              fontSize: 14,
-              color: CREAMWHITE,
-            }}
-          >
-            ${item.pricePerMeasuringUnit} per {item.measuringUnit}
-          </Text>
-        </View>
-      </TouchableOpacity>
+      <BuyerItemCard
+        key={item._id}
+        item={item}
+        location={location}
+        navigation={navigation}
+      />
     );
   }
 };
