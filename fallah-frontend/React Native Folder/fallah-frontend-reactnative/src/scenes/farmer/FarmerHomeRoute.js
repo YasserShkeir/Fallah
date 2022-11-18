f8ecba75c9ca5dc224468542594c86014a88a72d
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-18 18:14:54 +0200

    Frontend: Farmer-> Scene Home Route

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/scenes/farmer/FarmerHomeRoute.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/scenes/farmer/FarmerHomeRoute.js
@@ -1,17 +1,35 @@
 import { useState, useEffect } from "react";
-import { View } from "react-native";
-import { Menu, Dialog, Portal, Button, TextInput } from "react-native-paper";
+import { View, ScrollView } from "react-native";
+import {
+  Dialog,
+  Portal,
+  Button,
+  TextInput,
+  Text,
+  ActivityIndicator,
+} from "react-native-paper";
 
+// Components
+import ItemCard from "../../components/cards/ItemCard";
+
+// Hooks
+import { getSelfProducts } from "../../hooks/farmerItem";
 import { addLocation } from "../../hooks/locations";
 
 // Styles
-import { CREAMWHITE, LIGHTGREEN } from "../../styles/colors";
+import {
+  CREAMWHITE,
+  LIGHTGREEN,
+  DARKGREEN,
+  PEACHYYELLOW,
+} from "../../styles/colors";
 
 const FarmerHomeRoute = ({ navigation }) => {
   const [showDialog, setShowDialog] = useState(false);
   const [locationName, setLocationName] = useState("");
   const [locationLat, setLocationLat] = useState("");
   const [locationLong, setLocationLong] = useState("");
+  const [products, setProducts] = useState([]);
 
   const openDialog = () => setShowDialog(true);
   const closeDialog = () => setShowDialog(false);
@@ -25,16 +43,94 @@ const FarmerHomeRoute = ({ navigation }) => {
     await addLocation(location);
   };
 
+  const getSelfProductsHandler = (response) => {
+    setProducts(response.data.products);
+  };
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await getSelfProducts(getSelfProductsHandler);
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    prepare();
+  }, []);
+
   return (
     <View>
-      <Button
-        mode="contained"
-        icon={"pin"}
-        style={{ margin: 10, backgroundColor: LIGHTGREEN }}
-        onPress={openDialog}
+      <View
+        style={{
+          borderBottomColor: LIGHTGREEN,
+          borderBottomWidth: 1,
+          padding: 10,
+          paddingHorizontal: 15,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
       >
-        Add Location
-      </Button>
+        <Button
+          style={{
+            backgroundColor: PEACHYYELLOW,
+            width: "45%",
+          }}
+          labelStyle={{
+            color: DARKGREEN,
+            fontFamily: "Inter-Bold",
+          }}
+          onPress={() => navigation.navigate("FarmerAddItem")}
+        >
+          Add Product
+        </Button>
+        <Button
+          mode="contained"
+          icon={"pin"}
+          style={{ backgroundColor: LIGHTGREEN, width: "45%" }}
+          labelStyle={{
+            color: CREAMWHITE,
+            fontFamily: "Inter-Bold",
+          }}
+          onPress={openDialog}
+        >
+          Add Location
+        </Button>
+      </View>
+      <View
+        style={{
+          padding: 10,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 20,
+            fontFamily: "Inter-Bold",
+          }}
+        >
+          Your Products
+        </Text>
+        <ScrollView>
+          {products ? (
+            products.map((mainCat) => {
+              return mainCat.childCategories.map((childCat) => {
+                return childCat.products.map((item) => {
+                  return (
+                    <ItemCard
+                      key={item._id}
+                      item={item}
+                      navigation={navigation}
+                    />
+                  );
+                });
+              });
+            })
+          ) : (
+            <ActivityIndicator />
+          )}
+        </ScrollView>
+      </View>
       <Portal>
         <Dialog
           visible={showDialog}
