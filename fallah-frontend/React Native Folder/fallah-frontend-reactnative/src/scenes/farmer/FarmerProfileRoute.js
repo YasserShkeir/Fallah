413978c081c46db33d6b094f1f101f6a532539d0
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-18 03:48:14 +0200

    Frontend: Farmer-> Scene Profile Route

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/scenes/farmer/FarmerProfileRoute.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/scenes/farmer/FarmerProfileRoute.js
@@ -1,7 +1,39 @@
-import { Text } from "react-native-paper";
+import { View } from "react-native";
+import { Text, Button } from "react-native-paper";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { CREAMWHITE } from "../../styles/colors";
 
 const FarmerProfileRoute = ({ navigation }) => {
-  return <Text>Farmer Profile</Text>;
+  return (
+    <View>
+      <Button
+        mode="contained"
+        icon={() => (
+          <FontAwesome5 name="sign-out-alt" size={18} color={CREAMWHITE} />
+        )}
+        buttonColor="red"
+        style={{ width: 140 }}
+        contentStyle={{
+          flexDirection: "row-reverse",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        labelStyle={{
+          fontFamily: "Inter-Bold",
+          color: CREAMWHITE,
+          fontSize: 18,
+        }}
+        onPress={async () => {
+          // TODO: Logout
+          await AsyncStorage.removeItem("token");
+          navigation.navigate("SignIn");
+        }}
+      >
+        Logout
+      </Button>
+    </View>
+  );
 };
 
 export default FarmerProfileRoute;
