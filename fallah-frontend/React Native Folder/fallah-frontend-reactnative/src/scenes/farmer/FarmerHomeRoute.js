f8533afeaa84ff8ea82591799e7eac85cc59af6c
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-18 06:47:31 +0200

    Frontend: Farmer-> Scene Home Route Add Location

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/scenes/farmer/FarmerHomeRoute.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/scenes/farmer/FarmerHomeRoute.js
@@ -1,52 +1,84 @@
-import { View, TouchableOpacity } from "react-native";
-import { Text } from "react-native-paper";
-import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5";
+import { useState, useEffect } from "react";
+import { View } from "react-native";
+import { Menu, Dialog, Portal, Button, TextInput } from "react-native-paper";
+
+import { addLocation } from "../../hooks/locations";
 
 // Styles
 import { CREAMWHITE, LIGHTGREEN } from "../../styles/colors";
 
-const FarmerHomeRouterButton = ({ icon, label, navigation, to }) => {
-  return (
-    <TouchableOpacity
-      style={{
-        backgroundColor: LIGHTGREEN,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: 10,
-        margin: 10,
-        borderRadius: 10,
-      }}
-      onPress={() => {
-        navigation.navigate(to);
-      }}
-    >
-      <FontAwesome5Icon name={icon} size={20} color="white" />
-      <Text
-        style={{
-          color: CREAMWHITE,
-          fontFamily: "Inter-Bold",
-          fontSize: 20,
-          marginLeft: 10,
-        }}
-      >
-        {label}
-      </Text>
-      <FontAwesome5Icon name="chevron-right" size={20} color="white" />
-    </TouchableOpacity>
-  );
-};
-
 const FarmerHomeRoute = ({ navigation }) => {
+  const [showDialog, setShowDialog] = useState(false);
+  const [locationName, setLocationName] = useState("");
+  const [locationLat, setLocationLat] = useState("");
+  const [locationLong, setLocationLong] = useState("");
+
+  const openDialog = () => setShowDialog(true);
+  const closeDialog = () => setShowDialog(false);
+
+  const addLocationHandler = async (response) => {
+    let location = {
+      name: locationName,
+      latitude: locationLat,
+      longitude: locationLong,
+    };
+    await addLocation(location);
+  };
+
   return (
     <View>
-      <FarmerHomeRouterButton
-        icon="plus"
-        label="Add Location"
-        navigation={navigation}
-        to="food-apple"
-      />
+      <Button
+        mode="contained"
+        icon={"pin"}
+        style={{ margin: 10, backgroundColor: LIGHTGREEN }}
+        onPress={openDialog}
+      >
+        Add Location
+      </Button>
+      <Portal>
+        <Dialog
+          visible={showDialog}
+          onDismiss={closeDialog}
+          style={{
+            backgroundColor: CREAMWHITE,
+          }}
+        >
+          <Dialog.Title>Add a Location</Dialog.Title>
+          <Dialog.Content>
+            <TextInput
+              mode="outlined"
+              label="Location Name"
+              onChangeText={(text) => {
+                setLocationName(text);
+              }}
+            />
+            <TextInput
+              mode="outlined"
+              label="Location Longitude"
+              onChangeText={(text) => {
+                setLocationLong(text);
+              }}
+            />
+            <TextInput
+              mode="outlined"
+              label="Location Latitude"
+              onChangeText={(text) => {
+                setLocationLat(text);
+              }}
+            />
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button
+              onPress={async () => {
+                await addLocationHandler();
+                closeDialog();
+              }}
+            >
+              Confirm
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
     </View>
   );
 };
