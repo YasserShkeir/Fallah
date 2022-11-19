feb7cca35935001047d5ef6ab53576dfc5bccebd
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-19 16:34:37 +0200

    Frontend: Buyer-> Component Card Order Profile Item

---
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/cards/BuyerOrderProfileItemCard.js
@@ -0,0 +1,76 @@
+import { View } from "react-native";
+import { Button, Text } from "react-native-paper";
+
+// Hooks
+import { removeProductFromRegularOrder } from "../../hooks/buyerOrders";
+
+// Styles
+import { CREAMWHITE, LIGHTGREEN, PEACHYYELLOW } from "../../styles/colors";
+
+const BuyerOrderProfileItemCard = ({ order, product, navigation }) => {
+  console.log("product", navigation);
+  return (
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        marginVertical: 5,
+        marginHorizontal: 20,
+        borderRadius: 10,
+        backgroundColor: LIGHTGREEN,
+      }}
+    >
+      <View
+        style={{
+          width: "95%",
+          display: "flex",
+          flexDirection: order.deliveryStatus === "pending" ? "column" : "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "Inter-Bold",
+            fontSize: 16,
+            color: CREAMWHITE,
+          }}
+        >
+          {product.productName}
+        </Text>
+        <Text
+          style={{
+            fontFamily: "Inter-Medium",
+            fontSize: 14,
+            color: CREAMWHITE,
+          }}
+        >
+          Subtotal: ${product.productTotal}
+        </Text>
+      </View>
+      {order.deliveryStatus === "pending" ? (
+        <Button
+          mode="contained"
+          icon="delete"
+          contentStyle={{ backgroundColor: "red" }}
+          labelStyle={{ color: "white", fontFamily: "Inter-Bold" }}
+          onPress={async () => {
+            await removeProductFromRegularOrder(order._id, product._id).then(
+              (response) => {
+                console.log("response", response);
+              }
+            );
+          }}
+        >
+          Remove
+        </Button>
+      ) : null}
+    </View>
+  );
+};
+
+export default BuyerOrderProfileItemCard;
