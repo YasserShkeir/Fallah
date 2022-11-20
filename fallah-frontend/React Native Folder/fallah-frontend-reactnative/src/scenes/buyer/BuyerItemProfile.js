867064967f487972b4b605c54ff21e573f18c4a5
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-21 00:12:04 +0200

    Frontend: Buyer-> Scene Item Profile Add Components

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/scenes/buyer/BuyerItemProfile.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/scenes/buyer/BuyerItemProfile.js
@@ -1,25 +1,20 @@
 import { useState, useEffect } from "react";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
-import DropDownPicker from "react-native-dropdown-picker";
 
 // Components
 import BuyerItemFooter from "../../components/navbars/BuyerItemProfileFooter";
 import ProductTopSection from "../../components/sections/Buyer/Products/ProductTopSection";
+import ProductDetailsSection from "../../components/sections/Buyer/Products/ProductDetailsSection";
+import ProductOrderSection from "../../components/sections/Buyer/Products/ProductOrderSection";
 
 // Hooks
 import { getRegularOrders } from "../../hooks/buyerOrders";
-
-// Styles
-import { CREAMWHITE, DARKGREEN, LIGHTGREEN } from "../../styles/colors";
-import { flexRow } from "../../styles/components";
 
 const BuyerItemProfile = ({ route, navigation }) => {
   const { product } = route.params;
 
   const [regularOrders, setRegularOrders] = useState([]);
   const [selectedOrder, setSelectedOrder] = useState(regularOrders[0]);
-  const [orderMenuVisible, setOrderMenuVisible] = useState(false);
   const [quantity, setQuantity] = useState(1);
   const [payload, setPayload] = useState({
     regularOrderID: selectedOrder,
@@ -80,93 +75,14 @@ const BuyerItemProfile = ({ route, navigation }) => {
       }}
     >
       <ProductTopSection product={product} />
-      <View
-        style={{
-          ...flexRow,
-          borderBottomColor: LIGHTGREEN,
-          borderBottomWidth: 2,
-          paddingVertical: 5,
-          width: "100%",
-          paddingHorizontal: 10,
-          justifyContent: "space-between",
-        }}
-      >
-        <Text
-          style={{
-            fontFamily: "Inter-Medium",
-            color: DARKGREEN,
-            fontSize: 16,
-            marginVertical: 4,
-          }}
-        >
-          Harvested{" "}
-          {(
-            (new Date() -
-              new Date(
-                new Date(product.harvestedOn).setFullYear(
-                  new Date().getFullYear()
-                )
-              )) /
-            86400000
-          ).toFixed(0)}{" "}
-          days ago
-        </Text>
+      <ProductDetailsSection product={product} quantity={quantity} />
+      <ProductOrderSection
+        regularOrders={regularOrders}
+        selectedOrder={selectedOrder}
+        setSelectedOrder={setSelectedOrder}
+        setRegularOrders={setRegularOrders}
+      />
 
-        <Text
-          style={{
-            fontFamily: "Inter-Medium",
-            color: DARKGREEN,
-            fontSize: 16,
-            marginVertical: 4,
-          }}
-        >
-          {product.amountAvailable - quantity}
-          {product.measuringUnit} available
-        </Text>
-      </View>
-      <View
-        style={{
-          borderBottomColor: LIGHTGREEN,
-          borderBottomWidth: 2,
-          paddingVertical: 10,
-          width: "100%",
-          paddingHorizontal: 10,
-          backgroundColor: DARKGREEN,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        {regularOrders.length > 0 ? (
-          <DropDownPicker
-            containerStyle={{
-              width: "100%",
-            }}
-            textStyle={{ fontFamily: "Inter-Regular" }}
-            placeholder="Select Order"
-            open={orderMenuVisible}
-            value={selectedOrder}
-            items={regularOrders}
-            setOpen={setOrderMenuVisible}
-            setValue={setSelectedOrder}
-            setItems={setRegularOrders}
-            onChangeValue={(value) => {
-              setSelectedOrder(value);
-            }}
-          />
-        ) : (
-          <Text
-            style={{
-              fontFamily: "Inter-Regular",
-              color: CREAMWHITE,
-              fontSize: 18,
-            }}
-          >
-            No Active orders yet
-          </Text>
-        )}
-      </View>
       <BuyerItemFooter
         item={product}
         setQuantity={setQuantity}
