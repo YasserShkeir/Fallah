4b3dfd81e63db9aaad58c8873596a5a7e307a6ed
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-20 07:28:19 +0200

    Frontend: Buyer-> Scene Search Add Refresh Control

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/scenes/buyer/BuyerSearchRoute.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/scenes/buyer/BuyerSearchRoute.js
@@ -1,21 +1,23 @@
 import { useState, useEffect } from "react";
-import { FlatList } from "react-native";
+import { FlatList, RefreshControl } from "react-native";
 
 // Components
 import BuyerMainLayout from "../../components/layouts/BuyerMainLayout";
 import BuyerAppBar from "../../components/appbars/BuyerAppBar";
 import BuyerSearchCard from "../../components/cards/BuyerSearchCard";
-import BuyerSearchType from "../../components/sections/Search/BuyerSearchType";
+import BuyerSearchType from "../../components/sections/Buyer/Search/BuyerSearchType";
 
 // Hooks
 import { getSearchProducts } from "../../hooks/buyerCategories";
 import { getFarmers } from "../../hooks/buyerFarmer";
+import { PEACHYYELLOW } from "../../styles/colors";
 
 const BuyerSearchRoute = ({ navigation }) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [farmers, setFarmers] = useState([]);
   const [products, setProducts] = useState([]);
   const [value, setValue] = useState("Farmers");
+  const [loading, setLoading] = useState(true);
 
   const onChangeSearch = (query) => setSearchQuery(query);
 
@@ -28,6 +30,7 @@ const BuyerSearchRoute = ({ navigation }) => {
   };
 
   useEffect(() => {
+    setLoading(true);
     async function prepare() {
       try {
         await getFarmers(getFarmersHandler);
@@ -37,6 +40,7 @@ const BuyerSearchRoute = ({ navigation }) => {
       }
     }
     prepare();
+    setLoading(false);
   }, []);
 
   return (
@@ -48,6 +52,21 @@ const BuyerSearchRoute = ({ navigation }) => {
         setSearchQuery={setSearchQuery}
       />
       <FlatList
+        refreshControl={
+          <RefreshControl
+            colors={[PEACHYYELLOW]}
+            refreshing={loading}
+            onRefresh={() => {
+              setLoading(true);
+              {
+                value === "Farmers"
+                  ? getFarmers(getFarmersHandler)
+                  : getSearchProducts(getProductsHandler);
+              }
+              setLoading(false);
+            }}
+          />
+        }
         style={{ paddingHorizontal: 10, paddingVertical: 5 }}
         data={value === "Farmers" ? farmers : products}
         renderItem={({ item }) => {
@@ -78,7 +97,7 @@ const BuyerSearchRoute = ({ navigation }) => {
                 <BuyerSearchCard
                   key={item._id}
                   item={item}
-                  location={"search"}
+                  location="search"
                   navigation={navigation}
                 />
               );
@@ -92,7 +111,7 @@ const BuyerSearchRoute = ({ navigation }) => {
                 <BuyerSearchCard
                   key={item._id}
                   item={item}
-                  location={"search"}
+                  location="search"
                   navigation={navigation}
                 />
               );
