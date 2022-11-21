add8f1dfc3d395b3fc51ca7f5eb19ff4bbff18b3
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-21 19:13:42 +0200

    Frontend: Structure-> Delete Images from Frontend Side

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/assets/images/index.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/assets/images/index.js
@@ -1,32 +1,97 @@
-const images = [
+const DOWNLOAD_LINK = "http://10.0.2.2:3000/api/download/";
+
+const categoriesOptions = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Pragma: "cache",
+    "Cache-Control": "max-age=86400",
+  },
+};
+
+const miscOptions = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Pragma: "no-cache",
+    "Cache-Control": "no-cache",
+  },
+};
+
+export const categoriesImages = [
   {
     name: "Apples",
-    src: require("./apples/allApples.jpg"),
+    src: {
+      uri: `${DOWNLOAD_LINK}apples/allApples.jpg`,
+      ...categoriesOptions,
+    },
   },
   {
     name: "Bananas",
-    src: require("./bananas/allBananas.jpg"),
+    src: {
+      uri: `${DOWNLOAD_LINK}bananas/allBananas.jpg`,
+      ...categoriesOptions,
+    },
   },
   {
     name: "Lettuce",
-    src: require("./lettuce/allLettuce.jpg"),
+    src: {
+      uri: `${DOWNLOAD_LINK}lettuce/allLettuce.jpg`,
+      ...categoriesOptions,
+    },
   },
   {
     name: "Oranges",
-    src: require("./oranges/allOranges.jpg"),
+    src: {
+      uri: `${DOWNLOAD_LINK}oranges/allOranges.jpg`,
+      ...categoriesOptions,
+    },
   },
   {
     name: "Potatoes",
-    src: require("./potatoes/allPotatoes.jpg"),
+    src: {
+      uri: `${DOWNLOAD_LINK}potatoes/allPotatoes.jpg`,
+      ...categoriesOptions,
+    },
   },
   {
     name: "Onions",
-    src: require("./onions/allOnions.jpg"),
+    src: {
+      uri: `${DOWNLOAD_LINK}onions/allOnions.jpg`,
+      ...categoriesOptions,
+    },
   },
   {
     name: "View All",
-    src: require("./viewAllIcon.png"),
+    src: {
+      uri: `${DOWNLOAD_LINK}misc/viewAllIcon.png`,
+      ...categoriesOptions,
+    },
   },
 ];
 
-export default images;
+export const miscImages = [
+  {
+    name: "Dark Logo",
+    src: {
+      uri: `${DOWNLOAD_LINK}misc/DarkLogo.png`,
+      ...miscOptions,
+    },
+  },
+  {
+    name: "Light Login Logo",
+    src: {
+      uri: `${DOWNLOAD_LINK}misc/LightLoginLogo.png`,
+      ...miscOptions,
+    },
+  },
+  {
+    name: "Login BG",
+    src: {
+      uri: `${DOWNLOAD_LINK}misc/loginBG.jpg`,
+      ...miscOptions,
+    },
+  },
+];
