19798011e30519c8f0d99c11af1b323f34572f4f
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-21 02:40:54 +0200

    Frontend: Buyer-> Section Product Profile Top Add Portal

--- fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/sections/Buyer/Products/ProductTopSection.js
+++ fallah-frontend/React Native Folder/fallah-frontend-reactnative/src/components/sections/Buyer/Products/ProductTopSection.js
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import { View, Image, ScrollView, Dimensions } from "react-native";
-import { Text, Portal, Dialog, Button } from "react-native-paper";
+import { Text } from "react-native-paper";
 import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5";
+
+// Components
+import BuyerProductProfilePortal from "../../../dialogs/BuyerProductProfilePortal";
 
 // Styles
 import {
@@ -87,59 +90,11 @@ const ProductTopSection = ({ product }) => {
             }}
           />{" "}
         </Text>
-        <Portal>
-          <Dialog
-            visible={visible}
-            onDismiss={closeDialog}
-            style={{
-              backgroundColor: CREAMWHITE,
-            }}
-          >
-            <Dialog.Title>
-              <Text
-                style={{
-                  fontFamily: "Inter-Medium",
-                  color: DARKGREEN,
-                  fontSize: 22,
-                  marginVertical: 10,
-                }}
-              >
-                Discount Eligible Product!
-              </Text>
-            </Dialog.Title>
-            <Dialog.Content>
-              <Text
-                style={{
-                  fontFamily: "Inter-Regular",
-                  color: DARKGREEN,
-                  fontSize: 18,
-                }}
-              >
-                Buying more than {product.minBulkAmount}
-                {product.measuringUnit} allows you to benefit from a discounted
-                price of just ${product.bulkPrice}/{product.measuringUnit}!
-              </Text>
-            </Dialog.Content>
-            <Dialog.Actions>
-              <Button
-                mode="flat"
-                onPress={closeDialog}
-                labelStyle={{
-                  fontFamily: "Inter-Medium",
-                  fontSize: 16,
-                  color: CREAMWHITE,
-                }}
-                contentStyle={{
-                  backgroundColor: LIGHTGREEN,
-                  paddingHorizontal: 10,
-                }}
-                textColor={CREAMWHITE}
-              >
-                Thank you!
-              </Button>
-            </Dialog.Actions>
-          </Dialog>
-        </Portal>
+        <BuyerProductProfilePortal
+          visible={visible}
+          closeDialog={closeDialog}
+          product={product}
+        />
       </View>
     </>
   );
