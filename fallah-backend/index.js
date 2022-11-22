55bcfe9769f93ddbc2167fa9b9b4a301fce8934d
Yasser Shkeir <shkeiryasser@gmail.com>
2022-11-22 08:54:12 +0200

    Backend: Index-> Image Upload Add Filename as a response

--- fallah-backend/index.js
+++ fallah-backend/index.js
@@ -21,16 +21,16 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-app.get("/", (req, res) => {
-  res.status(200).send("You can post to /api/upload.");
-});
-
 app.post("/api/upload", upload.array("photo", 3), (req, res) => {
-  console.log("file", req.files);
-  console.log("body", req.body);
-  res.status(200).json({
-    message: "success!",
-  });
+  const fileName = req.files[0].path.split("\\").slice(-1)[0]; // Get the file name
+  try {
+    res.status(200).json({
+      message: fileName,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 app.get("/api/download/:route/:id", (req, res) => {
